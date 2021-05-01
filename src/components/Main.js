33cc7b9d3c3b0e9c951f9b4cdb7e94f774f5281e
Konstantin Brovtsev <brovan@yandex.ru>
2021-05-02 01:39:59 +0700

    add popup open handle functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,15 @@ function Main() {
     return (
         <main className="content page__content">
             <section className="profile content__profile">
-                <div className="profile__photo"></div>
+                <div className="profile__photo" onClick={handleEditAvatarClick}></div>
                 <div className="profile__container">
                     <div className="profile__name-containter">
                         <h1 className="profile__name">Жак-Ив Кусто</h1>
-                        <button type="button" className="profile__edit-button"></button>
+                        <button type="button" className="profile__edit-button" onClick={handleProfieEditClick}></button>
                     </div>
                     <p className="profile__job">Исследователь океана</p>
                 </div>
-                <button type="button" className="profile__plus-button"></button>
+                <button type="button" className="profile__plus-button" onClick={handleAddPlaceClick}></button>
             </section>
             <section className="cards content__cards">
                 <ul className="cards__list">
@@ -18,6 +18,18 @@ function Main() {
             </section>
         </main>
     );
+}
+
+function handleEditAvatarClick() {
+    document.querySelector('.popup_content_avatar-renew').classList.add('popup_opened');
+}
+
+function handleProfieEditClick() {
+    document.querySelector('.popup_content_profile-edit').classList.add('popup_opened');
+}
+
+function handleAddPlaceClick() {
+    document.querySelector('.popup_content_card-add').classList.add('popup_opened');
 }
 
 export default Main;
